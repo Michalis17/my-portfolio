5daf5735865021314d9157585ad0673494c6b891
Michalis <mike.michaelides17@gmail.com>
2024-06-04 13:39:35 +0300

    height issue fix + cursor for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import CursorFollower from "./components/cursorFollower/CursorFollower"
 
 const App: React.FC = ()=> {
   return (
-    <div className="bg-my-black flex justify-center ">
+    // display: flex;
+    // flex-direction: row;
+    // align-items: center;
+    // justify-content: center;
+    // height: 100%; add break point in md: to be h-screen
+    <div className="bg-my-black flex flex-row items-center justify-center h-full md:h-screen">
       <CursorFollower/>
       <PortfolioPage/>
     </div>
