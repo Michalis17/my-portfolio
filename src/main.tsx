b5a01004699c0286543b9ce96924e881824462d3
Michalis <mike.michaelides17@gmail.com>
2024-10-17 12:31:39 +0100

    added speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from './App.tsx'
 import './index.css'
 import './styles/tailwind.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>,
 )
